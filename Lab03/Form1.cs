2cae65dfb03d98e821ec266c2407276664104b57
Alex Ramos <aletsoshi@outlook.com>
2022-09-05 22:12:10 -0500

    Agregar archivos de proyecto.

---
+++ Lab03/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+
+namespace Lab03
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            //Declaramos variables para almacenar los valores de los TextBox
+            //y definimos una cadena de conexión
+            String servidor = txtServidor.Text;
+            String bd = txtBD.Text;
+            String user = txtUsuario.Text;
+            String pwd = txtPassword.Text;
+
+            String str = "Server=" + servidor + ";Database=" + bd + ";";
+            //La cadena de conexión cambia en función del estado del tijeckbox
+            if (chkAutenticacion.Checked)
+                str += "Integrated Security=true";
+            else
+                str += "User Id=" + user + "; Password=" + pwd + ";";
+            //Abrir una conexión con el servidor, usando la cadena de conexión
+            try
+            {
+                conn = new SqlConnection(str);
+                conn.Open();
+                MessageBox.Show("Conexion exitosa");
+                btnDesconectar.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar: " + ex.ToString());
+            }
+        }
+
+        private void btnEstado_Click(object sender, EventArgs e)
+        {
+            //Intentamos obtener el estado de la conexión, y en caso esté abierta,
+            // recuperamos información de la misma
+            try
+            {
+                if (conn.State == ConnectionState.Open)
+                    MessageBox.Show("Estado del servidor: " + conn.State +
+                    "/nVersión del servidor: " + conn.ServerVersion +
+                    "/nBase de datos: " + conn.Database);
+                else
+                    MessageBox.Show("Estado del servidor:"+ conn.State);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Imposible determinar el estado del servidor: \n" +
+                    ex.ToString());
+            }
+
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            //Para cerrar la conexion verificamos que no este cerrada
+            try
+            {
+                if (conn.State != ConnectionState.Closed)
+                {
+                    conn.Close();
+                    MessageBox.Show("Conexion cerrada satisfactoriamente");
+                }
+                else
+                    MessageBox.Show("La conexion ya esta cerrada");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrio un error al cerrar la conexion: /n" 
+                    + ex.ToString());
+            }
+        }
+
+        private void chkAutenticacion_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chkAutenticacion.Checked)
+            {
+                txtUsuario.Enabled = false;
+                txtPassword.Enabled = false;
+            }
+            else
+            {
+                txtUsuario.Enabled = true;
+                txtPassword.Enabled = true;
+            }
+        }
+
+        private void btnPersona_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona(conn);
+            persona.Show();
+        }
+    }
+}
